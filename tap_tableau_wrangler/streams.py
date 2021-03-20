548aceccb098ec7b486ad30e6cd0ca6317d80e8f
Ken Payne <ken@tails.com>
2021-03-20 11:41:20 +0000

    Only keep the most recent workbook ids.

--- tap_tableau_wrangler/streams.py
+++ tap_tableau_wrangler/streams.py
@@ -67,7 +67,6 @@ class WorkbookIds(TableauWorkbookFile):
     """
 
     name = 'workbook_ids'
-    primary_keys = ['observed_at']
     schema_filepath = SCHEMAS_DIR / 'workbook_ids.json'
 
     def get_records(self, partition: Optional[dict]) -> Iterable[Dict[str, Any]]:
